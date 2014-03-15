e7192df8cc75c84db2e18781480515acf3c41177
Jonathan Lai <jonathanlai@eltux.net>
2014-03-14 22:12:32 -0700

    ex14 functions, digging into different parts of program

--- ex14WritingAndUsingFunctions/ex14.c
+++ ex14WritingAndUsingFunctions/ex14.c
@@ -7,7 +7,6 @@ void print_letters(char arg[]);
 void print_arguments(int argc, char *argv[])
 {
   int i = 0;
-
   for(i = 0; i < argc; i++){
     print_letters(argv[i]);
   }
@@ -21,7 +20,7 @@ void print_letters(char arg[])
     char ch = arg[i];
 
     if(can_print_it(ch)) {
-      printf("'%c' == %d", ch, ch);
+      printf("'%c' == %d", ch, ch); // character followed by signed decimal integer representation
     }
   }
 
@@ -30,11 +29,19 @@ void print_letters(char arg[])
 
 int can_print_it(char ch)
 {
-  return isalpha(ch) || isblank(ch);
+  return isalpha(ch) || isblank(ch); // true or false, it needs to be a character or a blank to proceed
 }
 
 int main(int argc, char *argv[])
 {
+  printf("this is the number or argc, or words given as input %d\n", argc);
   print_arguments(argc, argv);
   return 0;
 }
+
+/*Links explaining argc and argv
+ * http://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
+ * http://stackoverflow.com/questions/779910/should-i-use-char-argv-or-char-argv-in-c
+ *
+ */
+
